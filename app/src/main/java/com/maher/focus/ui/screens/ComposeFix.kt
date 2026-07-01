be0cba418da6a64fb09e2c7122d8c92c0d79a824
msellmimaher-glitch <msellmi.maher@gmail.com>
2026-07-01 15:57:01 -0400

    Create ComposeFix.kt

---
+++ app/src/main/java/com/maher/focus/ui/screens/ComposeFix.kt
@@ -0,0 +1,23 @@
+package com.maher.focus.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column as ComposeColumn
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Column(
+    modifier: Modifier = Modifier,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    ComposeColumn(
+        modifier = modifier,
+        verticalArrangement = verticalArrangement,
+        horizontalAlignment = horizontalAlignment,
+        content = content
+    )
+}
